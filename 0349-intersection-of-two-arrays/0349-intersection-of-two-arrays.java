708349b1efe47c5e32b35a2b21c9e7b1e2cbb7d9
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2024-11-04 18:47:59 +0530

    Time: 3 ms (52.04%), Space: 43.1 MB (61.6%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,5 +1,51 @@
 class Solution {
+
+    public int[] listToIntArray(List<Integer> list){
+        int[] arr = new int[list.size()];
+        for (int i=0; i < arr.length; i++){
+            arr[i] = list.get(i).intValue();
+        }
+        return arr;
+    }
+
     public int[] intersection(int[] nums1, int[] nums2) {
+
+        //USING HASHMAP -->3ms
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int ele:nums1){
+            int val = map.getOrDefault(ele,0);
+            map.put(ele,val+1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (int ele:nums2){
+            if (map.containsKey(ele)){
+                list.add(ele);
+                map.remove(ele);
+            }
+        }
+        //arraylist to int array
+        int[] res = listToIntArray(list);
+        return res;
+
+
+
+        //USING SET --> 3ms
+        // Set<Integer> set1 = new HashSet<>();
+        // Set<Integer> set2 = new HashSet<>();
+        // for (int ele : nums1){
+        //     set1.add(ele);
+        // }
+        // for (int ele:nums2){
+        //     set2.add(ele);
+        // }
+        // set1.retainAll(set2);
         
+        // int[] res = new int[set1.size()];
+        // int i=0;
+        // for (int ele:set1){
+        //     res[i++] = ele;
+        // }
+        // return res;
+
     }
 }
