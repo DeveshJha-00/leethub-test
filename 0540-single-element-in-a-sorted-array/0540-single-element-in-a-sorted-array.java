a1aa9ebeeaeb81bc45db7c78f670178fa27e6218
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2024-11-09 19:29:07 +0530

    Time: 0 ms (100%), Space: 50.4 MB (31.8%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,14 +1,36 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int low=0, high=nums.length-1;
-        while (low<high){
-            int mid = (low+high)/2;
-            if (nums[mid] >= nums[low]) {
-                if (nums[low] == nums[low+1]) low+=2;
-                else return nums[low];
+        int n = nums.length;
+
+        // OWN TRY -> BINARY SEARCH
+        // int low=0, high=n-1;
+        // while (low<high){
+        //     int mid = (low+high)/2;
+        //     if (nums[mid] >= nums[low]) {
+        //         if (nums[low] == nums[low+1]) low+=2;
+        //         else return nums[low];
+        //     }
+        //     else if (nums[mid] < nums[high]) high-=2;
+        // }
+        // return nums[low];
+
+
+        // ACTUAL BINARY SEARCH 
+        int low=1, high=n-2;
+        if (n==1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n-1] != nums[n-2]) return nums[n-1];
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if (nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
             }
-            else if (nums[mid] < nums[high]) high-=2;
+            //if odd index and same ele on left AND the single ele is on right half
+            if (mid%2==1 && nums[mid]==nums[mid-1] ||
+                mid%2==0 && nums[mid]==nums[mid+1]) low=mid+1;
+            else high=mid-1; 
         }
-        return nums[low];
+        return -1;
     }
 }
