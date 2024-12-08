99c5a450c1fc500020cc1df8953722aee5a03b6d
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2024-12-08 12:38:47 +0530

    Time: 57 ms (83.74%), Space: 42.7 MB (61.44%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -1,42 +1,54 @@
 class Solution {
 
-    public int beauty(String s){
-        // HashMap<Character,Integer> map = new HashMap<>();
-        // for (char c : str.toCharArray()){
-        //     map.put(c, map.getOrDefault(c,0)+1);
-        // }
-        // int max=Integer.MIN_VALUE;
-        // int min=Integer.MAX_VALUE;
-        // for (int value : map.values()){
-        //     if (value > max) max = value;
-        //     if (value < min) min = value;
-        // }
-        // return (max-min);
-        int[] arr = new int[26];
-        for (int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            arr[c-'a']++;
-        }
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i=0; i<26; i++){
-            if (arr[i]>0){
-                max = Math.max(max, arr[i]);
-                min = Math.min(min, arr[i]);
-            }
-        }
-        return (max-min);
-    }
+    // public int beauty(String s){
+    //     int[] arr = new int[26];
+    //     for (int i=0; i<s.length(); i++){
+    //         char c = s.charAt(i);
+    //         arr[c-'a']++;
+    //     }
+
+    //     int max = Integer.MIN_VALUE;
+    //     int min = Integer.MAX_VALUE;
+    //     for (int i=0; i<26; i++){
+    //         if (arr[i]>0){
+    //             max = Math.max(max, arr[i]);
+    //             min = Math.min(min, arr[i]);
+    //         }
+    //     }
+    //     return (max-min);
+    // }
 
     public int beautySum(String s) {
-        //generate all substrgs and add each's beauty
+        // //generate all substrgs and add each's beauty
+        // int totBeauty = 0;
+        // for (int i=0; i<s.length(); i++){
+        //     for (int j=i; j<s.length(); j++){
+        //         String substr = s.substring(i,j+1);
+        //         totBeauty += beauty(substr);
+        //     }
+        // }
+        // return totBeauty;
+
+        //WAYY OPTIMIZED -->
         int totBeauty = 0;
         for (int i=0; i<s.length(); i++){
+            int[] arr = new int[26];
             for (int j=i; j<s.length(); j++){
-                String substr = s.substring(i,j+1);
-                totBeauty += beauty(substr);
+                arr[s.charAt(j)-'a']++;
+
+                //now find beauty of current substr and add
+                int max = Integer.MIN_VALUE;
+                int min = Integer.MAX_VALUE;
+                for (int k=0; k<26; k++){
+                    if (arr[k] > 0){
+                        max = Math.max(max, arr[k]);
+                        min = Math.min(min, arr[k]);
+                    }
+                }
+                totBeauty += (max-min);   
             }
         }
+
         return totBeauty;
     }
 }
