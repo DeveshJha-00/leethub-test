a38f0de78c0498932505161937772b5872216b3f
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2024-12-26 21:49:39 +0530

    Time: 4 ms (100%), Space: 44.6 MB (74.73%) - LeetHub

---
+++ 0860-design-circular-queue/0860-design-circular-queue.java
@@ -0,0 +1,72 @@
+class MyCircularQueue {
+    private int[] queue;
+    private int front;
+    private int rear;
+    private int size;
+    private int capacity;
+
+    public MyCircularQueue(int k) {
+        this.queue = new int[k];
+        this.front = -1;
+        this.rear = -1;
+        this.size=0;
+        this.capacity=k;
+    }
+    
+    public boolean enQueue(int value) {
+        if (isFull()) return false;
+        if (isEmpty()){
+            front=0;
+            rear = (rear+1)%capacity;
+            queue[rear]=value;
+            size++;
+            return true;
+        }
+        rear = (rear+1)%capacity;
+        queue[rear] = value;
+        size++;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        if (front == rear){
+            front=-1;
+            rear=-1;
+            size--;
+            return true;
+        }
+        front = (front + 1) % capacity; 
+        size--;
+        return true;
+    }
+    
+    public int Front() {
+        if (isEmpty()) return -1;
+        return queue[front];
+    }
+    
+    public int Rear() {
+        if (isEmpty()) return -1;
+        return queue[rear];
+    }
+    
+    public boolean isEmpty() {
+        return (rear==-1 && front==-1);
+    }
+    
+    public boolean isFull() {
+        return (rear+1)%capacity==front;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
