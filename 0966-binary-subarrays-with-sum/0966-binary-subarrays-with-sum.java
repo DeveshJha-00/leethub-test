59521acbca5c18b54a4720efa880b25fe7781688
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2024-12-29 12:15:06 +0530

    Time: 1 ms (100%), Space: 47.4 MB (84.69%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,25 +1,37 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-
-        //COUNT SUBBARAYS WITH SUM=K APPROACH (CAN BE OPTIMIZED FUTHER CZ BINARY)
+    public int atmostkoddnum(int[] nums, int goal){
+        if (goal<0) return 0; //for fun(nums,goal-1) if goal==0 initally 
         int l=0, r=0, ct=0, sum=0, n=nums.length;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
-        for(int ele:nums){
-            sum += ele;
-            int rem = sum-goal;
-            if (map.containsKey(rem)){
-                ct += map.get(rem);
+        while (r<n){
+            sum += nums[r];
+            while (sum > goal){
+                sum -= nums[l];
+                l++;
             }
-            map.put(sum, map.getOrDefault(sum,0)+1);
+            ct += r-l+1;
+            r++;
         }
         return ct;
+    }
 
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        //COUNT SUBBARAYS WITH SUM=K APPROACH (CAN BE OPTIMIZED FUTHER CZ BINARY)
+        // int l=0, r=0, ct=0, sum=0, n=nums.length;
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // map.put(0,1);
+        // for(int ele:nums){
+        //     sum += ele;
+        //     int rem = sum-goal;
+        //     if (map.containsKey(rem)){
+        //         ct += map.get(rem);
+        //     }
+        //     map.put(sum, map.getOrDefault(sum,0)+1);
+        // }
+        // return ct;
 
-
-
-
-
+        //FIND SUBARRAYS WITH SUM<=K AND THEN DO fun(nums,goal)-fun(nums,goal-1);
+        return atmostkoddnum(nums,goal)-atmostkoddnum(nums,goal-1);
+        
 
         //BRUTE FORCE
         // int n=nums.length, ct=0;
