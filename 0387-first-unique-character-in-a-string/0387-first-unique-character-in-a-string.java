fa290be605714fe47357a8c20c5917b2bdfeba80
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-01-13 10:11:06 +0530

    Time: 17 ms (58.2%), Space: 51.1 MB (6.28%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,36 +1,36 @@
 class Solution {
     public int firstUniqChar(String s) {
-        // Queue<Integer> queue = new LinkedList<>();
-        // int n = s.length();
-        // int[] freq = new int[26];
+        Queue<Integer> queue = new LinkedList<>();
+        int n = s.length();
+        int[] freq = new int[26];
 
-        // for (int i=0; i<n; i++){
-        //     char ch = s.charAt(i);
-        //     freq[ch-'a']++;
-        //     queue.offer(i);
-        // }
-
-        // while (!queue.isEmpty()) {
-        //     int index = queue.peek();
-        //     char c = s.charAt(index);
-        //     if (freq[c - 'a'] == 1) {
-        //         return index; 
-        //     }
-        //     queue.poll(); 
-        // }
-        // return -1;
-
-        //BETTER -->
-        int[] count = new int[26];
-        for(int i = 0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
+        for (int i=0; i<n; i++){
+            char ch = s.charAt(i);
+            freq[ch-'a']++;
+            queue.offer(i);
         }
-        for(int i = 0;i<s.length();i++){
-            if(count[s.charAt(i)-'a'] == 1){
-                return i;
+
+        while (!queue.isEmpty()) {
+            int index = queue.poll();
+            char c = s.charAt(index);
+            if (freq[c - 'a'] == 1) {
+                return index; 
             }
+            // queue.poll(); 
         }
         return -1;
+
+        //BETTER -->
+        // int[] count = new int[26];
+        // for(int i = 0;i<s.length();i++){
+        //     count[s.charAt(i)-'a']++;
+        // }
+        // for(int i = 0;i<s.length();i++){
+        //     if(count[s.charAt(i)-'a'] == 1){
+        //         return i;
+        //     }
+        // }
+        // return -1;
 
     }
 }
