5b967d6f12e50bbf3a8f2bcb4dc2d03b64436779
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-02-01 19:01:01 +0530

    Time: 0 ms (100%), Space: 43.4 MB (56.64%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -2,10 +2,11 @@ class Solution {
     public boolean isArraySpecial(int[] nums) {
         if (nums.length == 1) return true;
         for (int i = 0; i < nums.length-1; i++) {
-            int first = nums[i];
-            int second = nums[i+1];
-            if ((first % 2 == 0) && (second % 2 == 0)) return false;
-            else if ((first % 2 != 0) && (second % 2 != 0)) return false;
+            // int first = nums[i];
+            // int second = nums[i+1];
+            // if ((first % 2 == 0) && (second % 2 == 0)) return false;
+            // else if ((first % 2 != 0) && (second % 2 != 0)) return false;
+            if (nums[i]%2 == nums[i+1]%2) return false;
         }
 
         return true;
