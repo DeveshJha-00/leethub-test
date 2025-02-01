85f843da5a1b655db12cbde01d513c9efae11614
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-02-01 14:10:42 +0530

    Time: 798 ms (5.42%), Space: 44.7 MB (86.21%) - LeetHub

--- 1273-compare-strings-by-frequency-of-the-smallest-character/1273-compare-strings-by-frequency-of-the-smallest-character.java
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/1273-compare-strings-by-frequency-of-the-smallest-character.java
@@ -3,7 +3,6 @@ class Solution {
     public int func(String s){ //frequency of the lexicographically smallest character
         char smallest=s.charAt(0);
         int freq=1;  
-        // int 
         for (int i=1; i<s.length(); i++){
             if (s.charAt(i) == smallest) freq++;
             if (s.charAt(i) < smallest){
@@ -14,17 +13,23 @@ class Solution {
         return freq; 
     }
 
+    public boolean isValid(String query, String word){
+        return (func(query)<func(word));
+    }
+
     public int[] numSmallerByFrequency(String[] queries, String[] words) {
         int[] ans = new int[queries.length];
+
         for (int i=0; i<queries.length; i++){
             String query = queries[i];
             int ct=0;
             for (int j=0; j<words.length; j++){
                 String word=words[j];
-                if (func(query) < func(word)) ct++;
+                if (isValid(query,word)) ct++;
             }
             ans[i]=ct;
         }
+
         return ans;
     }
 
