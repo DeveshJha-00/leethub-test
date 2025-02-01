f5fb08286d0b096adc006758b4094ec70b112236
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-02-01 14:14:50 +0530

    Time: 3 ms (88.67%), Space: 44.6 MB (93.6%) - LeetHub

--- 1273-compare-strings-by-frequency-of-the-smallest-character/1273-compare-strings-by-frequency-of-the-smallest-character.java
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/1273-compare-strings-by-frequency-of-the-smallest-character.java
@@ -1,36 +1,84 @@
+// class Solution {
+
+//     public int func(String s){ 
+//         char smallest=s.charAt(0);
+//         int freq=1;  
+//         for (int i=1; i<s.length(); i++){
+//             if (s.charAt(i) == smallest) freq++;
+//             if (s.charAt(i) < smallest){
+//                 smallest=s.charAt(i);
+//                 freq=1;
+//             }
+//         }
+//         return freq; 
+//     }
+
+//     public int[] numSmallerByFrequency(String[] queries, String[] words) {
+//         int[] ans = new int[queries.length];
+
+//         for (int i=0; i<queries.length; i++){
+//             String query = queries[i];
+//             int ct=0;
+//             for (int j=0; j<words.length; j++){
+//                 String word=words[j];
+//                 if (func(query) < func(word)) ct++;
+//             }
+//             ans[i]=ct;
+//         }
+        
+//         return ans;
+//     }
+// }
+
 class Solution {
 
-    public int func(String s){ //frequency of the lexicographically smallest character
-        char smallest=s.charAt(0);
-        int freq=1;  
-        for (int i=1; i<s.length(); i++){
-            if (s.charAt(i) == smallest) freq++;
-            if (s.charAt(i) < smallest){
-                smallest=s.charAt(i);
-                freq=1;
+    public int func(String s) { 
+        char smallest = s.charAt(0);
+        int freq = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == smallest) {
+                freq++;
+            } else if (s.charAt(i) < smallest) {
+                smallest = s.charAt(i);
+                freq = 1;
             }
         }
-        return freq; 
-    }
-
-    public boolean isValid(String query, String word){
-        return (func(query)<func(word));
+        return freq;
     }
 
     public int[] numSmallerByFrequency(String[] queries, String[] words) {
+        int[] wordFreq = new int[words.length];
+        
+        // Precompute frequency values for words
+        for (int i = 0; i < words.length; i++) {
+            wordFreq[i] = func(words[i]);
+        }
+
+        Arrays.sort(wordFreq); // Sort to enable binary search
+        
         int[] ans = new int[queries.length];
 
-        for (int i=0; i<queries.length; i++){
-            String query = queries[i];
-            int ct=0;
-            for (int j=0; j<words.length; j++){
-                String word=words[j];
-                if (isValid(query,word)) ct++;
-            }
-            ans[i]=ct;
+        for (int i = 0; i < queries.length; i++) {
+            int queryFreq = func(queries[i]);
+
+            // Binary search for the first index where wordFreq[j] > queryFreq
+            int count = wordFreq.length - binarySearch(wordFreq, queryFreq);
+            ans[i] = count;
         }
 
         return ans;
     }
 
+    public int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 }
