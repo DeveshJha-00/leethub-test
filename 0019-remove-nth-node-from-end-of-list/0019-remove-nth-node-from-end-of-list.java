33b754250f40e3dd0151e063b77dd0d84cd6f146
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-02-04 12:35:07 +0530

    Time: 0 ms (100%), Space: 41.7 MB (74.64%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,41 +10,63 @@
  */
 class Solution {
 
-    public ListNode reverseList(ListNode head) {
-        if (head==null) return null;
-        if (head.next==null) return head;
+    //APPROACH 1-> REVERSE, REMOVE NTH, REVERSE AGAIN AND RETURN (DONE)
+    //APPROACH 2-> TRAVERSE TILL END AND FIND LENGTH (CT), TRAVERSE AGAIN TILL CT-N 
+    //              AND REMOVE THAT NODE
+    //APPROACH 3-> FAST AND SLOW PTR, MOVE FAST TILL NTH POSITION, THEN MOVE BOTH BY 1 EACH STEP
 
-        ListNode prev = null;
-        while (head != null){
-            ListNode nextNode = head.next;
-            head.next=prev; //reverse the link
-            prev=head; //move prev forward
-            head=nextNode; //move head forward
-        }
-        return prev;
-    }
+    // public ListNode reverseList(ListNode head) {
+    //     if (head==null) return null;
+    //     if (head.next==null) return head;
+
+    //     ListNode prev = null;
+    //     while (head != null){
+    //         ListNode nextNode = head.next;
+    //         head.next=prev; //reverse the link
+    //         prev=head; //move prev forward
+    //         head=nextNode; //move head forward
+    //     }
+    //     return prev;
+    // }
+
+    // public ListNode removeNthFromEnd(ListNode head, int n) {
+    //     if (head==null || head.next==null) return null;
+
+    //     //reverse
+    //     ListNode prev=reverseList(head);
+
+    //     ListNode temp = prev;
+    //     ListNode preTemp=prev;
+    //     int ct=1;
+    //     if (n==1){
+    //         prev=prev.next;
+    //     }else{
+    //         while (ct!=n){
+    //             preTemp=temp;
+    //             temp=temp.next;
+    //             ct++;
+    //         }
+    //         preTemp.next=temp.next;
+    //     }
+
+    //     //reverse
+    //     return reverseList(prev);
+    // }
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head==null || head.next==null) return null;
-
-        //reverse
-        ListNode prev=reverseList(head);
-
-        ListNode temp = prev;
-        ListNode preTemp=prev;
-        int ct=1;
-        if (n==1){
-            prev=prev.next;
-        }else{
-            while (ct!=n){
-                preTemp=temp;
-                temp=temp.next;
-                ct++;
-            }
-            preTemp.next=temp.next;
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i=0; i<n; i++) fast=fast.next;
+        if (fast == null){
+            head=head.next;
+            return head;
         }
-
-        //reverse
-        return reverseList(prev);
+        while (fast.next!=null && fast!=null){
+            slow=slow.next;
+            fast = fast.next;
+        }
+        
+        slow.next=slow.next.next;
+        return head;
     }
 }
