0bc2b120f30cb5f3f7192a301fcf7aba063ab6b8
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-02-06 17:41:43 +0530

    Time: 25 ms (73.77%), Space: 57.3 MB (13.91%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,26 +1,26 @@
-class Solution {
+class Solution { 
+    //DRIVER FN
     public int[] sortArray(int[] nums) {
         mergeSort(nums,0,nums.length-1);
         return nums;
     }
 
-    public void mergeSort(int[] arr, int si, int ei){
+    public void mergeSort(int[] arr, int low, int high){
+        if (low>=high) return;
+        int mid = low + (high-low)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
 
-        if (si>=ei) return;
-        int mid = si + (ei-si)/2;
-        mergeSort(arr,si,mid);
-        mergeSort(arr,mid+1,ei);
-
-        merge(arr,si,mid,ei);
+        merge(arr,low,mid,high);
 
     }
 
-    public void merge(int[] arr, int si, int mid, int ei){
-        int[] mergedArr = new int[ei-si+1];
-        int p1=si, p2=mid+1;
+    public void merge(int[] arr, int low, int mid, int high){
+        int[] mergedArr = new int[high-low+1];
+        int p1=low, p2=mid+1;
         int ptr=0;
 
-        while(p1<=mid && p2<=ei){
+        while(p1<=mid && p2<=high){
             if (arr[p1] <= arr[p2]){
                 mergedArr[ptr++]=arr[p1++];
             }else{
@@ -29,9 +29,9 @@ class Solution {
         }
 
         while(p1<=mid) mergedArr[ptr++]=arr[p1++];
-        while (p2<=ei) mergedArr[ptr++]=arr[p2++];
+        while(p2<=high) mergedArr[ptr++]=arr[p2++];
 
-        for (int i=0,j=si; i<mergedArr.length;i++,j++){
+        for (int i=0,j=low; i<mergedArr.length; i++,j++){
             arr[j] = mergedArr[i];
         }
     }
