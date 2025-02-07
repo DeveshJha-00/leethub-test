484d6730dc982edd7b011d4fd8b621b1e021c71f
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-02-07 23:47:27 +0530

    Time: 7 ms (65.12%), Space: 48.7 MB (17.29%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,33 +1,55 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> ans = new ArrayList<>();
-         Map<String, Integer> map = new HashMap<>();
+        Map<String, List<String>> hm = new HashMap<>();
 
-         for (int i=0; i<strs.length; i++){
-            String word=strs[i];
-            String key = getCode(word);
+        for(int i=0; i<strs.length; i++){
+            char[] ch = strs[i].toCharArray();
+            Arrays.sort(ch);
+            String sorted = new String(ch);
 
-            if (map.containsKey(key)){ //add in existing sub-list
-                int val=map.get(key);
-                ans.get(val).add(word);
-            }else{
-                map.put(key, ans.size()); //create new sub-list
-                List<String> newGroup = new ArrayList<>();
-                newGroup.add(word);  
-                ans.add(newGroup);
+            if(hm.get(sorted) == null){
+                hm.put(sorted, new ArrayList<String>());
             }
-         }
+            hm.get(sorted).add(strs[i]);
+        }
 
+        for(List<String> val : hm.values()){
+            ans.add(val);
+        }
+        
         return ans;
     }
-    public String getCode(String word){
-        StringBuilder code = new StringBuilder();
-        int[] freq = new int[26];
-        for (char ch : word.toCharArray()) freq[ch-'a']++;
-        for (int i=0; i<26; i++){
-            code.append(freq[i]);
-            code.append('#');
-        }
-        return code.toString();
-    }
+
+    // public List<List<String>> groupAnagrams(String[] strs) {
+    //     List<List<String>> ans = new ArrayList<>();
+    //      Map<String, Integer> map = new HashMap<>();
+
+    //      for (int i=0; i<strs.length; i++){
+    //         String word=strs[i];
+    //         String key = getCode(word);
+
+    //         if (map.containsKey(key)){ //add in existing sub-list
+    //             int val=map.get(key);
+    //             ans.get(val).add(word);
+    //         }else{
+    //             map.put(key, ans.size()); //create new sub-list
+    //             List<String> newGroup = new ArrayList<>();
+    //             newGroup.add(word);  
+    //             ans.add(newGroup);
+    //         }
+    //      }
+
+    //     return ans;
+    // }
+    // public String getCode(String word){
+    //     StringBuilder code = new StringBuilder();
+    //     int[] freq = new int[26];
+    //     for (char ch : word.toCharArray()) freq[ch-'a']++;
+    //     for (int i=0; i<26; i++){
+    //         code.append(freq[i]);
+    //         code.append('#');
+    //     }
+    //     return code.toString();
+    // }
 }
