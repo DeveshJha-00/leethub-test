f5787c94b4e5a0d7e98660ec00cd2dd021e0b1d5
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-03-22 11:21:34 +0530

    Time: 1 ms (99.97%), Space: 45.7 MB (78.97%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -5,7 +5,7 @@ class Solution {
         int rightmostSetBit = xor & -(xor);
         int num1=0, num2=0;
         for (int ele:nums){
-            if ((ele & rightmostSetBit) != 0) num1 ^= ele;
+            if ((ele & rightmostSetBit) == 0) num1 ^= ele;
             else num2 ^= ele;
         }
         return new int[] {num1, num2};
