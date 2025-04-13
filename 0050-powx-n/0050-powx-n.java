128bcedb537897cf184ec2ea822dc78ee4a2b58e
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-04-13 20:25:19 +0530

    Time: 0 ms (100%), Space: 42 MB (81.09%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -4,15 +4,15 @@ class Solution {
         if (n<0){ //EDGE CASE/S
             x=1/x;
             long N = -(long)n; //IF n=INT_MIN
-            return myPow(x, N);
+            return powerFn(x, N);
         }
-        return myPow(x,(long)n);
+        return powerFn(x,(long)n);
     }
 
     //MAIN BINARY EXPONENTIATION FN.
-    public double myPow(double x, long n){
+    public double powerFn(double x, long n){
         if (n==0) return 1;
-        double half = myPow(x,n/2);
+        double half = powerFn(x,n/2);
         double res = half*half;
         if (n%2==1) res=res*x;
         return res;
