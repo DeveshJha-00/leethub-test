39c944e9e4f9d8f35dfe29127236393816868eae
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-07-06 13:05:35 +0530

    Time: 43 ms (20.13%), Space: 44.6 MB (44.78%) - LeetHub

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -0,0 +1,80 @@
+class Solution {
+
+    public long subArrayRanges(int[] nums) {
+        return sumSubarrayMax(nums) - sumSubarrayMins(nums);
+    }
+    
+    public long sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int[] pse = prevSmallerOrEqualEle(arr);
+        int[] nse = nextSmallerEle(arr);
+        long total=0;
+        for (int i=0; i<n; i++){
+            total += (long) (i-pse[i])*(nse[i]-i) * arr[i];
+        }
+        return total;
+    }
+    public long sumSubarrayMax(int[] arr) {
+        int n = arr.length;
+        int[] pge = prevGreaterOrEqualEle(arr);
+        int[] nge = nextGreaterEle(arr);
+        long total=0;
+        for (int i=0; i<n; i++){
+            total += (long) (i-pge[i])*(nge[i]-i) * arr[i];
+        }
+        return total;
+    }
+    public int[] prevSmallerOrEqualEle(int[] arr){
+        int n = arr.length;
+        int[] pse = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i=0; i<n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return pse;
+    }
+
+    public int[] nextSmallerEle(int[] arr){
+        int n = arr.length;
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=n-1; i>=0; i--){
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            nse[i] = st.isEmpty() ? n : st.peek(); 
+            st.push(i);
+        }
+        return nse;
+    }
+    public int[] prevGreaterOrEqualEle(int[] arr){
+        int n = arr.length;
+        int[] pge = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i=0; i<n; i++){
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]){
+                st.pop();
+            }
+            pge[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return pge;
+    }
+    public int[] nextGreaterEle(int[] arr){
+        int n = arr.length;
+        int[] nge = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i=n-1; i>=0; i--){
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i]){
+                st.pop();
+            }
+            nge[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        return nge;
+    }
+}
