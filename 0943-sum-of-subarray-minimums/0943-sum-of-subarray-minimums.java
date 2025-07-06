8f19bbdd484aa20a53ef0d6c23f13fa7a8094a3d
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-07-06 12:19:35 +0530

    Time: 74 ms (25.99%), Space: 51 MB (66.85%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.java
@@ -1,57 +1,57 @@
 class Solution {
 
-    // public int[] prevSmallerOrEqualEle(int[] arr){
-    //     int n = arr.length;
-    //     int[] pse = new int[n];
-    //     Stack<Integer> st = new Stack<>();
-    //     for (int i=0; i<n; i++) {
-    //         while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
-    //             st.pop();
-    //         }
-    //         pse[i] = st.isEmpty() ? -1 : st.peek();
-    //         st.push(i);
-    //     }
-    //     return pse;
-    // }
-
-    // public int[] nextSmallerEle(int[] arr){
-    //     int n = arr.length;
-    //     int[] nse = new int[n];
-    //     Stack<Integer> st = new Stack<>();
-    //     for(int i=n-1; i>=0; i--){
-    //         while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-    //             st.pop();
-    //         }
-    //         nse[i] = st.isEmpty() ? n : st.peek(); 
-    //         st.push(i);
-    //     }
-    //     return nse;
-    // }
-
-    public int sumSubarrayMins(int[] arr) {
+    public int[] prevSmallerOrEqualEle(int[] arr){
         int n = arr.length;
-        int[] nse = new int[n];
         int[] pse = new int[n];
         Stack<Integer> st = new Stack<>();
-        for (int i=0; i<n; i++) { //MAKING PSE(LEFT)
+        for (int i=0; i<n; i++) {
             while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
                 st.pop();
             }
             pse[i] = st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
-        st.clear();
+        return pse;
+    }
 
-        for(int i=n-1; i>=0; i--){ //MAKING RIGHT
+    public int[] nextSmallerEle(int[] arr){
+        int n = arr.length;
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=n-1; i>=0; i--){
             while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
                 st.pop();
             }
             nse[i] = st.isEmpty() ? n : st.peek(); 
             st.push(i);
         }
+        return nse;
+    }
 
-        // int[] nse = nextSmallerEle(arr);
-        // int[] pse = prevSmallerOrEqualEle(arr);
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        // int[] nse = new int[n];
+        // int[] pse = new int[n];
+        // Stack<Integer> st = new Stack<>();
+        // for (int i=0; i<n; i++) { //MAKING PSE(LEFT)
+        //     while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+        //         st.pop();
+        //     }
+        //     pse[i] = st.isEmpty() ? -1 : st.peek();
+        //     st.push(i);
+        // }
+        // st.clear();
+
+        // for(int i=n-1; i>=0; i--){ //MAKING RIGHT
+        //     while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+        //         st.pop();
+        //     }
+        //     nse[i] = st.isEmpty() ? n : st.peek(); 
+        //     st.push(i);
+        // }
+
+        int[] nse = nextSmallerEle(arr);
+        int[] pse = prevSmallerOrEqualEle(arr);
         long total=0;
         int mod=(int)(1e9+7);
         for (int i=0; i<n; i++){
