152cd0e83a17b847b467d84a99089ffedb34b655
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-07-25 13:32:57 +0530

    Time: 46 ms (60.77%), Space: 111.4 MB (92.33%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,34 +1,60 @@
-class LRUCache {
-    Map<Integer, Integer> map;
-    Deque<Integer> dq;
-    int capacity;
+// class LRUCache {
+//     Map<Integer, Integer> map;
+//     Deque<Integer> dq;
+//     int capacity;
+
+//     public LRUCache(int capacity) {
+//         map = new HashMap<>();
+//         dq = new ArrayDeque<>();
+//         this.capacity=capacity;
+//     }
+    
+//     public int get(int key) {
+//         if (map.containsKey(key)){
+//             dq.remove(key);       
+//             dq.offer(key);
+//             return map.get(key);
+//         }
+//         return -1;
+//     }
+    
+//     public void put(int key, int value) {
+//         if (map.containsKey(key)){
+//             dq.remove(key);
+//         }else{
+//             if (map.size()==capacity){
+//                 map.remove(dq.pollFirst());
+//             }
+//         }
+//         map.put(key, value);
+//         dq.offer(key);
+//     }
+// }
+
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private final int capacity;
+
     public LRUCache(int capacity) {
-        map = new HashMap<>();
-        dq = new ArrayDeque<>();
-        this.capacity=capacity;
+        // true = access order, false = insertion order
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
     }
-    
+
     public int get(int key) {
-        if (map.containsKey(key)){
-            dq.remove(key);       
-            dq.offer(key);
-            return map.get(key);
-        }
-        return -1;
+        return super.getOrDefault(key, -1);
     }
-    
+
     public void put(int key, int value) {
-        if (map.containsKey(key)){
-            dq.remove(key);
-        }else{
-            if (map.size()==capacity){
-                map.remove(dq.pollFirst());
-            }
-        }
-        map.put(key, value);
-        dq.offer(key);
+        super.put(key, value);
+    }
+
+    // Automatically remove eldest (LRU) when size exceeds capacity
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
     }
 }
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
