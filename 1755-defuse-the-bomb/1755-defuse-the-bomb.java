b0f0f1507bec187cd6a447fd25673f18c792ce36
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2025-08-06 12:09:07 +0530

    Time: 0 ms (100%), Space: 42.3 MB (72.31%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,27 +1,36 @@
 class Solution {
-    public int[] decrypt(int[] code, int k) {
-        int n = code.length;
+    public int[] decrypt(int[] arr, int k) {
+        int n=arr.length;
         int[] res = new int[n];
-        if (k==0){
-            for (int i=0; i<n; i++) res[i]=0;
-        }else if (k>0){
-            for (int i=0; i<n; i++){
-                int sum=0;
-                for(int j=0; j<k; j++){
-                    sum += code[(i+j+1)%n];
+
+        if(k>0){
+            int l=1, r=1, i=0, sum=0;
+            while(i!=n){
+                sum += arr[r];
+                if( (r-l+n) % n+1 == k){ //if size is k, mark as soln and move window
+                    res[i++]=sum;
+                    r=(r+1)%n;
+                    sum -= arr[l];
+                    l=(l+1)%n;
                 }
-                res[i]=sum;
-            }   
-        }else{
-            for (int i=0; i<n; i++){
-                int sum=0;
-                for(int j=n-1; j>=(n+k); j--){
-                    sum += code[(j-1+i+1)%n]; // or sum+=code[(j+i)%n]
-                }
-                res[i]=sum;
-            } 
-        }
+                else r=(r+1)%n;
+            }
             
+        }
+        else if(k<0){
+            int l=n-2, r=n-2 ,i=n-1, sum=0;
+            while(i!=-1){
+                sum += arr[l];
+                if((r - l + n) % n + 1 == Math.abs(k)){
+                    res[i--]=sum;
+                    l=(l-1+n)%n;
+                    sum -= arr[r];
+                    r=(r-1+n)%n;
+                }
+                else l=(l-1+n)%n;
+            }
+        }
+
         return res;
     }
 }
