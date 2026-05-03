3c6353c01e2bb44ed98359f7178c1817864a8cee
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-05-03 12:00:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -2,13 +2,18 @@ class Solution {
     public int[] countOppositeParity(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        for (int i=0; i<n; i++){
-            int cnt = 0;
-            for (int j=i+1; j<n; j++){
-                if ((nums[i] & 1) != (nums[j] & 1)) cnt++;
+        int odd=0, even=0;
+        
+        for (int i=n-1; i>=0; i--){
+            if (nums[i] % 2 == 0){
+                even++;
+                res[i] = odd;
+            }else{
+                odd++;
+                res[i] = even;
             }
-            res[i] = cnt;
         }
+
         return res;
     }
 }
