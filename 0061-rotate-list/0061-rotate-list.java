fb4196eb1f474ef92bac53793706cd452869d5c2
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-05-05 21:44:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,43 +10,29 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if (head == null || head.next == null || k == 0) return head;
-        
         int n = 0;
         ListNode node = head;
         while(node != null){
-            node = node.next;
             n++;
+            node = node.next;
         }
 
         k = k%n;
-        ListNode temphead1 = reverseLL(head, 1, n);
-        System.out.println(temphead1.val);
-        ListNode temphead2 = reverseLL(temphead1, 1, k);
-        System.out.println(temphead2.val);
-        ListNode temphead3 = reverseLL(temphead2, k+1, n);
-
-        return temphead3;
-    }
-
-    public ListNode reverseLL(ListNode head, int left, int right){
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-
-        for (int i = 1; i < left; i++) {
-            prev = prev.next;
+        int l = n-k;
+        ListNode newTail = head;
+        ListNode newHead = null;
+        for (int i=1; i<l; i++){
+            newTail = newTail.next;
         }
+        newHead = newTail.next;
+        newTail.next = null;
 
-        ListNode curr = prev.next;
-        for (int i = 0; i < right - left; i++) {
-            ListNode temp = curr.next;
-            curr.next = temp.next;
-            temp.next = prev.next;
-            prev.next = temp;
+        ListNode temp = newHead;
+        while(temp.next != null){
+            temp = temp.next;
         }
+        temp.next = head;
 
-        return dummy.next;
+        return newHead;
     }
 }
-
