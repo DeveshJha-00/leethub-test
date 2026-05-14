7f32976ef3f3d7553f61bf57384bdc37b8cfb8c6
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-05-15 00:15:13 +0530

    Time: 1 ms (91.05%), Space: 44.6 MB (70.86%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,32 +1,32 @@
-// class Solution {
-//     public boolean isGood(int[] nums) {
-//         int maxEle = Integer.MIN_VALUE;
-//         for (int num : nums) maxEle=Math.max(maxEle, num);
-
-//         if (maxEle + 1 != nums.length) return false;
-
-//         int[] freq = new int[maxEle + 1];
-//         for (int i=0; i<nums.length; i++){
-//             freq[nums[i]]++;
-            
-//             if (nums[i] != maxEle && freq[nums[i]] > 1) return false;
-//             if (nums[i] == maxEle && freq[nums[i]] > 2) return false;
-//         }
-
-//         return true;
-//     }
-// }
-
 class Solution {
     public boolean isGood(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
+        int maxEle = Integer.MIN_VALUE;
+        for (int num : nums) maxEle=Math.max(maxEle, num);
 
-        for (int i = 0; i <= n - 2; i++) {
-            if (nums[i] != i + 1)
-                return false;
+        if (maxEle + 1 != nums.length) return false;
+
+        int[] freq = new int[maxEle + 1];
+        for (int i=0; i<nums.length; i++){
+            freq[nums[i]]++;
+            
+            if (nums[i] != maxEle && freq[nums[i]] > 1) return false;
+            if (nums[i] == maxEle && freq[nums[i]] > 2) return false;
         }
 
-        return nums[n - 1] == n - 1;
+        return true;
     }
 }
+
+// class Solution {
+//     public boolean isGood(int[] nums) {
+//         int n = nums.length;
+//         Arrays.sort(nums);
+
+//         for (int i = 0; i <= n - 2; i++) {
+//             if (nums[i] != i + 1)
+//                 return false;
+//         }
+
+//         return nums[n - 1] == n - 1;
+//     }
+// }
