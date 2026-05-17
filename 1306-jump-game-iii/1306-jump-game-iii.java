1a1f18596642dfd64b9a0d951fdcc80c45153009
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-05-17 17:07:53 +0530

    Time: 2 ms (82.42%), Space: 52.9 MB (28.33%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -5,23 +5,17 @@ class Solution {
     }
 
     private boolean solve(int[] arr, int idx, boolean[] visited){
+        int n = arr.length;
+        if (idx<0 || idx>=n || visited[idx]){
+            return false;
+        }
+
         if (arr[idx] == 0) return true;
 
-        int n = arr.length;
         visited[idx] = true;
-        int op1Idx = idx + arr[idx];
-        int op2Idx = idx - arr[idx];
+        boolean op1 = solve(arr, idx+arr[idx], visited);
+        boolean op2 = solve(arr, idx-arr[idx], visited);
 
-        boolean op1 = false;
-        boolean op2 = false;
-        if (op1Idx < n && !visited[op1Idx]){
-            op1 = solve(arr, op1Idx, visited);
-        }
-        if (op2Idx >=0 && !visited[op2Idx]){
-            op2 = solve(arr, op2Idx, visited);
-        }
-
-        // visited[idx] = false;
         return op1 || op2;
     }
 }
