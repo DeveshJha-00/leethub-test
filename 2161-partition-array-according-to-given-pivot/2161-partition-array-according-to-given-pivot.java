d7b2f6f6d1fa4e6b55474057894992e615372c28
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-06-08 10:07:44 +0530

    Time: 4 ms (99.24%), Space: 160 MB (49.83%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,27 +1,21 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> smaller = new ArrayList<>();
-        List<Integer> larger = new ArrayList<>();
-        int sameCnt = 0;
+        int cntLess = 0;
+        int cntSame = 0;
         for (int ele : nums){
-            if (ele == pivot) sameCnt++;
-            else if (ele > pivot) larger.add(ele);
-            else smaller.add(ele);
+            if (ele == pivot) cntSame++;
+            else if (ele < pivot) cntLess++;
         }
 
-        int ptr = 0;
-        while(ptr < nums.length){
-            for (int ele : smaller){
-                nums[ptr++]=ele;
-            }
-            for (int i=0; i<sameCnt; i++){
-                nums[ptr++] = pivot;
-            }
-            for (int ele : larger){
-                nums[ptr++] = ele;
-            }
+        int i=0, j=cntLess, k=cntLess+cntSame;
+
+        int[] res = new int[nums.length];
+        for (int ele : nums){
+            if (ele < pivot) res[i++] = ele;
+            else if (ele == pivot) res[j++] = ele;
+            else res[k++] = ele;
         }
 
-        return nums;
+        return res;
     }
 }
