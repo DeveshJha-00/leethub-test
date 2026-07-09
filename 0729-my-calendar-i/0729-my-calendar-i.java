a9ab7467e17372f4865476ea31d507f1f048039c
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-07-09 16:28:39 +0530

    Time: 347 ms (5.36%), Space: 47.8 MB (5.93%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -0,0 +1,29 @@
+class MyCalendar {
+    Map<Integer, Integer> map;
+
+    public MyCalendar() {
+        map = new TreeMap<>();    
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        map.put(startTime, map.getOrDefault(startTime, 0) + 1);
+        map.put(endTime, map.getOrDefault(endTime, 0) - 1);
+        int curr = 0;
+        for (int val : map.values()){
+            curr += val;
+            if (curr > 1){
+                map.put(startTime, map.get(startTime) - 1);
+                map.put(endTime, map.get(endTime) + 1);
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
