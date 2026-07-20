794104400f7d2db0f342299e9da900e1f021a383
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-07-20 21:08:35 +0530

    Time: 9 ms (16.09%), Space: 46.8 MB (96.55%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -9,31 +9,16 @@ class Solution {
             for (int j=0; j<n; j++) list.add(grid[i][j]);
         }
 
-        List<Integer> temp = new ArrayList<>();
-        for (int i=list.size()-k; i<list.size(); i++) temp.add(list.get(i));
-
-        List<Integer> ans = new ArrayList<>();
-
-        for (int i = 0; i < list.size(); i++) {
-            ans.add(0);
-        } // so no errors while using ans.get(i)
-
-        for (int i=0; i<k; i++) {
-            ans.set(i, temp.get(i));
-        }
-
-
-        for (int i=k; i<list.size(); i++) {
-            ans.set(i, list.get(i - k));
-        }
+        reverse(list, 0, list.size()-1);
+        reverse(list, 0, k-1);
+        reverse(list, k, list.size()-1);
 
         List<List<Integer>> res = new ArrayList<>();
         int idx = 0;
-
         for (int i=0; i<m; i++) {
             List<Integer> row = new ArrayList<>();
             for (int j = 0; j < n; j++) {
-                row.add(ans.get(idx++));
+                row.add(list.get(idx++));
             }
 
             res.add(row);
@@ -42,4 +27,13 @@ class Solution {
         return res;
 
     }   
+
+    public void reverse(List<Integer> list, int i, int j){
+        while (i < j){
+            int temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
+            i++; j--;
+        }
+    }
 }
