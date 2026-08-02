b7ea0949ac0e28609607acf3a173d87bff031a17
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-08-02 09:04:40 +0530

    Time: 10 ms (10.85%), Space: 49.5 MB (7.73%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,33 +1,56 @@
-class Solution {
-    Integer[][][] dp;
+// class Solution {
+//     Integer[][][] dp;
+//     int n;
+
+//     public boolean stoneGame(int[] piles) {
+//         this.n = piles.length;
+//         this.dp = new Integer[2][n+1][n+1];;
+//         return solveForAlice(piles, 0, n-1, 1) > 0; //return alice - bob
+//     }
+
+//     public int solveForAlice(int[] piles, int l, int r, int isAlice){
+//         if (l > r) return 0;
+
+//         if (dp[isAlice][l][r] != null) return dp[isAlice][l][r];
+
+//         int res;
+//         if (isAlice == 1) res=Integer.MIN_VALUE;
+//         else res=Integer.MAX_VALUE;
+
+//         int count = 0;
+//         if (isAlice == 1){
+//             int left = piles[l] - solveForAlice(piles, l+1, r, 0);
+//             int right = piles[r] - solveForAlice(piles, l, r-1, 0);
+//             res = Math.max(res, Math.max(left, right));
+//         }else{
+//             int left = solveForAlice(piles, l+1, r, 0);
+//             int right = solveForAlice(piles, l, r-1, 0);
+//             res = Math.min(res, Math.min(left, right));
+//         }
+
+//         return  dp[isAlice][l][r] = res;
+//     }
+// }
+
+class Solution{
     int n;
+    Integer[][] dp;
 
     public boolean stoneGame(int[] piles) {
         this.n = piles.length;
-        this.dp = new Integer[2][n+1][n+1];;
-        return solveForAlice(piles, 0, n-1, 1) > 0; //return alice - bob
+        this.dp = new Integer[n+1][n+1];
+
+        return solve(piles, 0, n-1) > 0;
     }
 
-    public int solveForAlice(int[] piles, int l, int r, int isAlice){
+    public int solve(int[] piles, int l, int r){
         if (l > r) return 0;
 
-        if (dp[isAlice][l][r] != null) return dp[isAlice][l][r];
+        if (dp[l][r] != null) return dp[l][r];
 
-        int res;
-        if (isAlice == 1) res=Integer.MIN_VALUE;
-        else res=Integer.MAX_VALUE;
+        int left = piles[l] - solve(piles, l+1, r);
+        int right = piles[r] - solve(piles, l, r-1);
 
-        int count = 0;
-        if (isAlice == 1){
-            int left = piles[l] - solveForAlice(piles, l+1, r, 0);
-            int right = piles[r] - solveForAlice(piles, l, r-1, 0);
-            res = Math.max(res, Math.max(left, right));
-        }else{
-            int left = solveForAlice(piles, l+1, r, 0);
-            int right = solveForAlice(piles, l, r-1, 0);
-            res = Math.min(res, Math.min(left, right));
-        }
-
-        return  dp[isAlice][l][r] = res;
+        return dp[l][r] = (int)Math.max(left, right);
     }
 }
