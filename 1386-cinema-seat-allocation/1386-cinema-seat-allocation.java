7928c0fd503c773e0e1189c960ce17c8f3b1e97e
DeveshJha-00 <164185962+DeveshJha-00@users.noreply.github.com>
2026-08-19 20:50:44 +0530

    Time: 55 ms (5.26%), Space: 60.1 MB (6.32%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,27 +1,34 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Map<Integer, HashSet<Integer>> map = new HashMap<>();
-        int res = 2 * n;
+        Map<Integer, HashSet<Integer>> map = new TreeMap<>();
+        int res = 0;
 
-        for (int[] seats : reservedSeats) {
-            int row = seats[0], seat = seats[1];
+        for (int[] seats : reservedSeats){
+            int row=seats[0], seat=seats[1];
+
             map.computeIfAbsent(row, k -> new HashSet<>()).add(seat);
         }
 
-        for (HashSet<Integer> seats : map.values()) {
-            boolean left = !seats.contains(2) && !seats.contains(3)
-                    && !seats.contains(4) && !seats.contains(5);
+        for (Map.Entry<Integer, HashSet<Integer>> e : map.entrySet()){
+            HashSet<Integer> seats = e.getValue();
+            if (!seats.contains(2) && !seats.contains(3) && !seats.contains(4) && !seats.contains(5)){
+                res++;
+                seats.add(2); seats.add(3); seats.add(4); seats.add(5);
+            }
 
-            boolean right = !seats.contains(6) && !seats.contains(7)
-                    && !seats.contains(8) && !seats.contains(9);
+            if (!seats.contains(6) && !seats.contains(7) && !seats.contains(8) && !seats.contains(9)){
+                res++;
+                seats.add(6); seats.add(7); seats.add(8); seats.add(9);
+            }
 
-            boolean middle = !seats.contains(4) && !seats.contains(5)
-                    && !seats.contains(6) && !seats.contains(7);
-
-            if (left && right) continue;
-            else if (left || right || middle) res--;
-            else res -= 2;
+            if (!seats.contains(4) && !seats.contains(5) && !seats.contains(6) && !seats.contains(7)){
+                res++;
+                // seats.add(4); seats.add(5); seats.add(6); seats.add(7);
+            }
         }
+
+        res += (n - map.size()) * 2;
+
 
         return res;
     }
